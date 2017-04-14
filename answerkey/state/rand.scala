9151ca5eae5972df2e794c55945d2ca1a1b6f4d9
Daniel Lau <danlaudk@yahoo.dk>
2017-04-13 22:28:11 -0400

    Update rand.scala

--- answerkey/state/rand.scala
+++ answerkey/state/rand.scala
@@ -5,10 +5,14 @@ trait RNG {
 
 
 object RNG {
+  // use : val rng = simple(42)
+  //       rng.nextInt //whenever u want ur next random int
  def simple(seed: Long): RNG = new RNG {
     def nextInt: (Int, RNG) = ??? // a particular simple pseudoRNG
  }
+  
  type Rand[+A] = RNG => (A, RNG)
 
  val int: Rand[Int] = _.nextInt
+ def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
  }
