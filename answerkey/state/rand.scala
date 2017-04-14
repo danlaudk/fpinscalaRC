fc5b6b62123dbc1711f7ab79c4deb38c8ee1e4b0
Daniel Lau <danlaudk@yahoo.dk>
2017-04-14 11:54:54 -0400

    Update rand.scala

--- answerkey/state/rand.scala
+++ answerkey/state/rand.scala
@@ -16,3 +16,17 @@ object RNG {
  val int: Rand[Int] = _.nextInt
  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
  }
+
+
+// given 
+def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+  rng => {
+    val (a, r1) = ra(rng)
+    val (b, r2) = rb(r1)
+    (f(a, b), r2)
+  }
+
+// sequence is the same as before
+
+// derive flatmap
+def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B]
